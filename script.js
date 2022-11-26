2b210909261e26fd72be2e62dd32165cc93fe8fd
Raazkatwal <raazkatwal5@gmail.com>
2022-11-26 10:44:27 +0545

    added dino and obstacle

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function jump(){
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    console.log(`left = ${blockLeft}, character top = ${characterTop}`);
+    if(blockLeft<-1 && blockLeft>-20 && characterTop>=230){
         block.style.animation = "none";
         alert("Game Over. score: "+Math.floor(counter/100));
         counter=0;
